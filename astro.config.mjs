c5faf64cce4205e567c864d134dbccd0f2964521
makhan <makhan@typingfactory.org>
2026-05-04 08:51:38 +0530

    feat: better fonts and more content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://saveandroid.github.io',
-  base: '/helpsaveandroid.org',
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: 'EB Garamond',
+    weights: [400, 500, 600, 700],
+    cssVariable: '--font-serif'
+  }],
 
   vite: {
     plugins: [tailwindcss()]
